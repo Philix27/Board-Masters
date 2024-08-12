eacaed0caa3fa2598ede8d300c18325009efea96
Philix27 <philixbob@gmail.com>
2024-08-12 21:07:03 +0100

    Remove game page and resource

--- apps/mini/app/(core)/game/store/useUser.ts
+++
@@ -1,8 +0,0 @@
-'use client';
-import { useRecoilValue } from 'recoil';
-import { userAtom } from './user';
-
-export const useUser = () => {
-  const value = useRecoilValue(userAtom);
-  return value;
-};
