c2792ddf95702291026290a6334a1bedea7001f1
Philix27 <philixbob@gmail.com>
2024-08-15 13:20:43 +0100

    fix paths error

--- mini/app/comps/theme/theme-toggle.tsx
+++ mini/app/comps/theme/theme-toggle.tsx
@@ -1,24 +1,20 @@
-"use client"
+'use client';
 
-import * as React from "react"
-import { Button } from "@/comps"
-import { Moon, Sun } from "lucide-react"
-import { useTheme } from "next-themes"
+import * as React from 'react';
+import { AppButton } from '@/comps';
+import { Moon, Sun } from 'lucide-react';
+import { useTheme } from 'next-themes';
 
 export function ThemeToggle() {
-  const { setTheme, theme } = useTheme()
+  const { setTheme, theme } = useTheme();
 
   return (
-    <Button
-      variant="ghost"
-      size="icon"
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-    >
-      {theme === "light" ? (
+    <AppButton variant="ghost" size="icon" onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
+      {theme === 'light' ? (
         <Moon className="h-[1.5rem] w-[1.3rem text-primary-foreground" />
       ) : (
         <Sun className="h-[1.5rem] w-[1.3rem text-primary-foreground" />
       )}
-    </Button>
-  )
+    </AppButton>
+  );
 }
--- mini/app/not-found.tsx
+++ mini/app/not-found.tsx
@@ -2,16 +2,16 @@
 
 import Link from "next/link"
 
-import { Button, TextB, TextH } from "./comps"
+import { AppButton, TextP, TextH } from "./comps"
 
 export default function NotFound() {
   return (
     <div className={"w-full flex items-center justify-center h-screen"}>
       <div className="flex flex-col items-center">
         <TextH  className="mb-4">Not Found</TextH>
-        <TextB className="mb-4"> Could not find requested resource</TextB>
+        <TextP className="mb-4"> Could not find requested resource</TextP>
         <Link href="/" >
-          <Button>Return Home</Button>
+          <AppButton>Return Home</AppButton>
         </Link>
       </div>
     </div>
